0e45d733a9a63ccf3e9ee0b1bb008beb803a3de4
gigant-misli <larinlovkach@gmail.com>
2022-09-12 13:03:14 +0400

    init commit

---
+++ src/index.js
@@ -0,0 +1,198 @@
+import './index.css';
+
+if (!Array.prototype.last){
+  Array.prototype.last = function(){
+      return this[this.length - 1];
+  };
+};
+
+if (!Array.prototype.first){
+  Array.prototype.first = function(){
+      return this[0];
+  };
+};
+
+if (!Array.prototype.choice){
+  Array.prototype.choice = function(){
+    var index = Math.floor(Math.random() * this.length);
+    return this[index];
+  };
+};
+
+const CELL_SIZE = 20;
+
+const GRID_WIDTH = 30;
+const GRID_HEIGHT = 30;
+
+const canvas = document.createElement('canvas');
+canvas.setAttribute('width', GRID_WIDTH * CELL_SIZE);
+canvas.setAttribute('height', GRID_HEIGHT * CELL_SIZE);
+document.body.appendChild(canvas);
+
+var ctx = canvas.getContext("2d"); //Получение контекста — через него можно работать с холстом
+
+
+function getRandomValue(min, max) {
+  return Math.floor(min + (Math.random() * (max - min)));
+}
+
+class Snake {
+  constructor() {
+    this.dx = getRandomValue(-1,1) || 1;
+    this.dy = getRandomValue(-1,1) && 0;
+    this.hx = getRandomValue(10, 20);
+    this.hy = getRandomValue(10, 20);
+    this.points = 0;
+    this.isCellAdded = false;
+    this.cells = [new Cell(this.hx, this.hy), new Cell(this.hx - this.dx, this.hy - this.dy)];
+  }
+
+  addCell() {
+    const firstCell = Object.assign(new Cell(), this.cells.first()); 
+    firstCell.x = firstCell.x + this.dx;
+    firstCell.y = firstCell.y + this.dy;
+    this.cells.unshift(firstCell)
+    this.isCellAdded = true;
+    console.log(this.cells)
+  }
+
+  draw() {
+    let lastCell = null;
+
+    this.cells.forEach(cell => {
+      if (!this.isCellAdded) {
+        if (lastCell) {
+          cell.x = lastCell.x;
+          cell.y = lastCell.y;
+        } else {
+          lastCell = Object.assign(new Cell(), cell);
+          this.hx = cell.x + this.dx;
+          this.hy = cell.y + this.dy;
+          cell.x = this.hx;
+          cell.y = this.hy;
+        }
+      }
+      cell.draw();
+    });
+
+    this.isCellAdded = false;
+  }
+
+  eat(apple) {
+    console.log('eat apple')
+    // this.addCell();
+    this.points++;
+    // const allCells = [];
+    
+    // for (let i = 0; i < GRID_WIDTH; i++) {
+    //   for (let j = 0; j < GRID_HEIGHT; j++) {
+    //     allCells.push({ x:i, y:j })
+    //   }
+    // }
+
+    // const emptyCells = allCells.filter(cell => this.cells.some(snakeCell => snakeCell.isIntersected(cell)));
+
+    // const emptyCell = emptyCells.choice();
+    
+    
+  }
+
+  isCrashed() {
+    for (let i = 4; i < this.cells.length; i++) {
+      if (this.cells[i].x === this.hx && this.cells[i].y === this.hy) {
+        return true;
+      }
+    }
+    if (this.hx === GRID_WIDTH || this.hx === 0 || this.hy === GRID_HEIGHT || this.hy === 0)
+      return true;
+    return false;
+  }
+}
+
+class Cell {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw() {
+    ctx.fillStyle="red";
+    ctx.fillRect(this.x * CELL_SIZE, this.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+  }
+
+  isIntersected(cell) {
+    return this.x === cell.x && this.y === cell.y;
+  }
+}
+
+class Apple {
+  constructor() {
+    this.x = getRandomValue(0, GRID_WIDTH - 1);
+    this.y = getRandomValue(0, GRID_HEIGHT - 1);
+  }
+
+  draw() {
+    ctx.fillStyle="green";
+    ctx.rect(this.x * CELL_SIZE, this.y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    ctx.fill();
+  }
+}
+
+class Game {
+  static Start()
+  {
+    const snake = new Snake();
+    const apple = new Apple();
+  
+    document.addEventListener('keydown', function (e) {
+      if (e.code === 'ArrowLeft' && snake.dx === 0) {
+        snake.dx = -1;
+        snake.dy = 0;
+      }
+      else if (e.code === 'ArrowUp' && snake.dy === 0) {
+        snake.dx = 0;
+        snake.dy = -1;
+      }
+      else if (e.code === 'ArrowRight' && snake.dx === 0) {
+        snake.dx = 1;
+        snake.dy = 0;
+      }
+      else if (e.code === 'ArrowDown' && snake.dy === 0) {
+        snake.dx = 0;
+        snake.dy = 1;
+      }
+    });
+    
+    this.timer = setInterval(() => this.Update(snake, apple), 1000 / 5); //Состояние игры будет обновляться 60 раз в секунду — при такой частоте обновление происходящего будет казаться очень плавным
+  }
+   
+  static Stop()
+  {
+    clearInterval(this.timer); //Остановка обновления
+  }
+   
+  static Update(snake, apple) //Обновление игры
+  {
+    this.Draw(snake, apple);
+  }
+   
+  static Draw(snake, apple) //Работа с графикой
+  {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); //Очистка холста от предыдущего кадра
+    // console.log(snake.hx, apple.x);
+    if (snake.hx === apple.x && snake.hy === apple.y) {
+      snake.eat(apple);
+      apple.x = getRandomValue(0, GRID_WIDTH - 1);
+      apple.y= getRandomValue(0, GRID_HEIGHT - 1);
+    }
+  
+    if (snake.isCrashed()) {
+      return this.Stop();
+    }
+  
+    apple.draw();
+    snake.draw();
+  }
+}
+
+Game.Start();
